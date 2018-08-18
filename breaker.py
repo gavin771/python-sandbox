7cdc78278e8ea3c157c302d0e554db7a145431d9
Gavin Samuels <gavin.samuels10@gmail.com>
2018-08-18 15:44:12 -0500

    finished script to open youtube video every 2 hours

--- breaker.py
+++ breaker.py
@@ -1,4 +1,8 @@
 import webbrowser
 import time
 
-webbrowser.open("https://www.youtube.com/watch?v=r6GMBCaagok")
+print("Started at {}".format(time.ctime()))
+for c in range(3):
+  time.sleep(2*60*60)
+  print("Break #{} at {}".format(c+1, time.ctime()))
+  webbrowser.open("https://www.youtube.com/watch?v=r6GMBCaagok")
